3272b506a836dc224c6344ec29819f4ecae89a1a
brenoza <brenojustino669@gmail.com>
2026-04-15 08:16:23 -0300

    Add files via upload

---
+++ Atividade.py
@@ -0,0 +1,8 @@
+def soma_segura(a, b):
+    
+    try:
+        return a + b
+
+    except:
+        print("Entrada recusada")
+        return 0
---
+++ Atividade2.py
@@ -0,0 +1,7 @@
+def divisao(x, y):
+    
+    try:
+        return x / y
+
+    except:
+        return "não e pra dividir por 0 não da"
